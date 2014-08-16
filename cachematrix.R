22ed3033bbe496a7ed743d636c8d578f4c1acfe0
nicus <lorenzo.nicora@gmail.com>
2014-08-16 15:26:12 +0200

    Implemented the 2 functions for the 2nd assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions may be used to hold a matrix together with 
+## its inverse
+## makeCacheMatrix is the container of the data
+## cacheSolve should be used to calculate/retrieve the inverse
 
-## Write a short comment describing this function
-
+## This function is a "container" holding a matrix and its inverse
+## It retuirns a list of functions (set/get, setinv/getinv)
+## respectively to store/retireve the matrix
+## and to store/retrieve the inverse
+## The matrix may be initialized on creation of a new instance, 
+## passing it as argument of makeCacheMatrix()
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    
+    # Store the matrix
+    set  <- function(mtx) {
+        x <<- mtx
+        i <<- NULL
+    }
+    
+    # Retrieve the matrix
+    get <- function() x
+    
+    # Store the inverse
+    setinv <- function(inv) i <<- inv
+    
+    # Retrieve the inverse
+    getinv <- function() i
+    
+    # Return a list of functions to access the content
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverse from a makeCacheMatrix instance
+## lazily calculating it only on the first call
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv  <- x$getinv()
+    if(!is.null(inv)) {
+        message("cache hit")
+        return(inv)
+    }
+    mtx <- x$get()
+    inv <- solve(mtx)
+    x$setinv(inv)
+    inv
 }
